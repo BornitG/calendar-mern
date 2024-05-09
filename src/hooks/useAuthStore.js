4a534ee0ea24590b60d8a5d87117756f1bb7b333
Bornit <alghenny@hotmail.com>
2024-05-09 19:31:06 -0400

    feat: Added onLogutCalend to the calendarSlice to clear the calendar initialState

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { calendarApi } from "../api";
-import { clearErrorMessage, onChecking, onLogin, onLogout } from "../store";
+import { clearErrorMessage, onChecking, onLogin, onLogout, onLogoutCalendar } from "../store";
 
 
 export const useAuthStore = () => {
@@ -54,7 +54,8 @@ export const useAuthStore = () => {
 
     const startLogout = () => {
         localStorage.clear();
-        dispatch(onLogout());
+        dispatch( onLogoutCalendar() );
+        dispatch( onLogout() );
     }
 
     return {
